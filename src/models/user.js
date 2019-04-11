d11ba1af6d1eedb0103ca3609c7e7f34d38fde63
Gabriel Vidal <gabrielvidals@gmail.com>
2019-04-10 21:56:44 -0400

    add user session feature

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+
+const PASSWORD_SALT = 10;
+
+async function buildPasswordHash(instance) {
+  if (instance.changed('password')) {
+    const hash = await bcrypt.hash(instance.password, PASSWORD_SALT);
+    instance.set('password', hash);
+  }
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: 'uniqueEmail',
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [6],
+      },
+    },
+  }, {});
+
+  user.beforeUpdate(buildPasswordHash);
+  user.beforeCreate(buildPasswordHash);
+
+  user.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+  return user;
+};
---
+++ src/routes/session.js
@@ -0,0 +1,30 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+router.get('session.new', '/new', ctx => ctx.render('session/new', {
+  createSessionPath: ctx.router.url('session.create'),
+  notice: ctx.flashMessage.notice,
+}));
+
+router.put('session.create', '/', async (ctx) => {
+  const { email, password } = ctx.request.body;
+  const user = await ctx.orm.user.find({ where: { email } });
+  const isPasswordCorrect = user && await user.checkPassword(password);
+  if (isPasswordCorrect) {
+    ctx.session.userId = user.id;
+    return ctx.redirect(ctx.router.url('courses.list'));
+  }
+  return ctx.render('session/new', {
+    email,
+    createSessionPath: ctx.router.url('session.create'),
+    error: 'Incorrect mail or password',
+  });
+});
+
+router.delete('session.destroy', '/', (ctx) => {
+  ctx.session = null;
+  ctx.redirect(ctx.router.url('session.new'));
+});
+
+module.exports = router;
